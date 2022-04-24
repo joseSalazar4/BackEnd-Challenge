06d68b901219eda23eef928362abc3ae15b8ed89
Jose <jsalazarg48@gmail.com>
2022-04-24 14:38:38 -0600

    Added route  method getSongsByArtist
    - the song is a param from the URL
    - the query string seems to be the same for this and upcoming methods
    - All songs that match the name will be retrieved

--- controller/songs.go
+++ controller/songs.go
@@ -42,3 +42,40 @@ func GetAllSongs(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": songs})
 }
+
+func GetSongsByArtist(c *gin.Context) {
+	queryResult, errFound := dbInstance.Query("SELECT genres.name AS genre, songs.length, songs.song, songs.artist " +
+		" FROM songs " +
+		" INNER JOIN genres " +
+		" ON genres.id = songs.ID " + " WHERE songs.artist = '" + c.Param("artist") + "'")
+
+	if errFound != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Problem retrieving songs data"})
+		return
+	}
+	songs := make([]model.Song, 0)
+
+	for queryResult.Next() {
+		song := model.Song{}
+		errFound = queryResult.Scan(&song.Genre, &song.Length, &song.Song, &song.Artist)
+		if errFound != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Problem retrieving songs data"})
+			return
+		}
+		songs = append(songs, song)
+
+	}
+
+	errFound = queryResult.Err()
+	if errFound != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Problem retrieving songs data"})
+		return
+	}
+
+	if songs == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Problem retrieving songs data"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": songs})
+}
