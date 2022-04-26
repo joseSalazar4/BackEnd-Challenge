ff30ca4c9bed6f0f9241dd525a53d32589fbd294
Jose <jsalazarg48@gmail.com>
2022-04-25 23:00:09 -0600

    Refactor router Setup into function for testing to be available
    - Refactored the router creation to be a function and
    -

--- main.go
+++ main.go
@@ -7,20 +7,27 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
+func SetupRouter() *gin.Engine {
 	// Generate the router instance
 	router := gin.Default()
 
 	// Connect to database and generate an instance to use
 	controller.ConnectToDatabase()
 
-	// These will be the only routes created
+	// These are the routes created containing main tasks and extra credit
 	router.GET("/songs", controller.GetAllSongs)
 	router.GET("/songsByLength", controller.GetSongsByLength)
 	router.GET("/songsByGenre/:genre", controller.GetSongsByGenre)
 	router.GET("/songsByArtist/:artist", controller.GetSongsByArtist)
 	router.GET("/songsSortedByGenre", controller.GetSongsStatsSortedByGenre)
 	router.GET("/songsBySongName/:nameSearched", controller.GetSongsBySongName)
+
+	return router
+}
+
+func main() {
+	// Call the func to connect to database and set routes!
+	router := SetupRouter()
 
 	// Start Listening! Port: 8080
 	router.Run()
